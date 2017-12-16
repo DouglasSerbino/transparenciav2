93d2fe0f75a2f63657f99bb84df91090743a3a3e
douglasserbino <dserbino@gmail.com>
2017-12-16 13:52:19 -0600

    Pagina de documentos inicial, Enlazado

--- transparencia/app/consultas/urls.py
+++ transparencia/app/consultas/urls.py
@@ -5,4 +5,5 @@ from app.consultas import views
 app_name = 'consultas'
 urlpatterns = [
    path('', views.index, name='inicio'),
+   path('documentos/', views.documentos, name='documentos'),
 ]
--- transparencia/app/consultas/views.py
+++ transparencia/app/consultas/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request,'Principal/index.html',{})
+
+def documentos(request):
+	return render(request, 'Principal/documentos.html',{})
